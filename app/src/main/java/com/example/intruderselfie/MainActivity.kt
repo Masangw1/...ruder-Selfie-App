c239a1e6f23df85e4424066bb9f87c8c81b902aa
Masangw1 <mmasangwi@gmail.com>
2026-08-04 06:06:18 +0200

    Add Intent import to MainActivity
    Fix: add missing Intent import

--- app/src/main/java/com/example/intruderselfie/MainActivity.kt
+++ app/src/main/java/com/example/intruderselfie/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.intruderselfie
 
 import android.Manifest
+import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Bundle
